e13f9c8c5e35606267f40f4decc0ba3fe6ce1802
Pedro-melo12 <pedrinhomelo2005@gmail.com>
2023-09-09 13:20:39 -0300

    criação dos atributos na classe post.

--- src/main/java/com/blog/model/post.java
+++ src/main/java/com/blog/model/post.java
@@ -1,5 +1,40 @@
 package com.blog.model;
 
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_posts")
 public class post {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idPost;
+
+    @Column(nullable = false, length = 255)
+    private String titulo;
+
+    @Column(columnDefinition = "TEXT")
+    private String conteudo;
+
+    @Column(nullable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataDeCriacao;
+
+    @Column(nullable = false)
+    private long idAutor;
+
 }
--- src/main/java/com/blog/model/usuario.java
+++ src/main/java/com/blog/model/usuario.java
@@ -1,10 +1,18 @@
 package com.blog.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_usuarios")
 public class usuario {
     
 
