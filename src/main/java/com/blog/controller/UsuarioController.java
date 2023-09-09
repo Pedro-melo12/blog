26bb3ecbd27f39687d60d4bc9a3de637c50eda33
Pedro-melo12 <pedrinhomelo2005@gmail.com>
2023-09-09 14:59:11 -0300

    Criação do CRUD na classe UsuarioCOntroller

--- src/main/java/com/blog/controller/UsuarioController.java
+++ src/main/java/com/blog/controller/UsuarioController.java
@@ -1,5 +1,75 @@
 package com.blog.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.blog.model.Usuario;
+import com.blog.repository.UsuarioRepository;
+
 public class UsuarioController {
-    
+     @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listarusuarios(Usuario usuario) {
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> listarusuariosPeloId(@PathVariable("id") Long id) {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+
+        if (usuario.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(usuario.get());
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> cadastrarCategoria(@RequestBody Usuario usuario) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuario));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizarCategoria(@PathVariable("id") Long id, @RequestBody Usuario usuario) {
+        Optional<Usuario> usuarioExistente = usuarioRepository.findById(id);
+
+        if (usuarioExistente.isPresent()) {
+            usuarioExistente.get().setNome(usuario.getNome());
+            usuarioExistente.get().setSobrenome(null);(usuario.getConteudo());
+            usuarioExistente.get().setDataDeCriacao(usuario.getDataDeCriacao());
+            usuarioExistente.get().setUsuario(usuario.getUsuario());
+
+            return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(usuarioExistente.get()));
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletarusuarioPeloId(@PathVariable Long id) {
+        Optional<usuario> usuario = usuarioRepository.findById(id);
+
+        if (usuario.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        usuarioRepository.deleteById(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("usuario deletada com sucesso!");
+    }
+
 }
+
+
